fd0c242f341f8773a52ef0aea490d28c0748154e
carolynemilgo <carolynemilgo@gmail.com>
2017-06-03 17:56:37 +0300

    updated hamburger menu and google analytics

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,20 @@
 //     alert("has been succesfully added to our email list");
 //   });
 // });
+function toggle_text_color_mouse_over()
+{
+var all = document.getElementByClassName('menuOptions');
+for(var i=0;i<all.length;++)
+{
+  all[i].style.color='black';
+}
+}
+
+function toggle_text_color_mouse_out()
+{
+var all = document.getElementByClassName('menuOptions');
+for(var i=0;i<all.length;++)
+{
+  all[i].style.color='black';
+}
+}
